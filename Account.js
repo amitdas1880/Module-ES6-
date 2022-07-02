c05cb6077c11bd8a9ade21d85d7f23805a422d06
Amit Kumar Das <102554819+amitdas1880@users.noreply.github.com>
2022-07-02 10:32:45 +0530

    Add files via upload

---
+++ Account.js
@@ -0,0 +1,21 @@
+export function account1 ( customer1 ,Account_no , Balance)
+    {
+ console.log (customer1 ,Account_no , Balance);
+}
+
+
+
+
+export function account2 (  customer1 ,Account_no , Balance )
+{
+    console.log (customer1 ,Account_no , Balance);
+}
+
+
+
+
+export function account3 (   customer1 ,Account_no , Balance )
+{
+    console.log (customer1 ,Account_no , Balance);
+}
+
---
+++ address.js
@@ -0,0 +1,19 @@
+function address1 ()
+{
+console.log ( " A/6 ramdev bagan , jamshedpur");
+}
+
+function address2 ()
+{
+console.log ("C/8 sundar nager, jamshedpur" );
+}
+
+
+function address3 ()
+{
+console.log ("U/8 baridih, jamshedpur");
+}
+
+let BANK_NAME = "BANK OF BARODA" ;
+
+export { address1, address2 , address3, BANK_NAME};                                     // shortcut key of export.
