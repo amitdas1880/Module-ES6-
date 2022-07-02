92724e66b8869dc7b7e51a1c8ca6e35983777df7
Amit Kumar Das <102554819+amitdas1880@users.noreply.github.com>
2022-07-02 10:34:46 +0530

    Add files via upload

---
+++ status.js
@@ -0,0 +1,17 @@
+export default function  ()                                         // we can define only one default function in every folder.
+{                                                                   // at the time of creation of default function , the name of the function is not define.
+    console.log ("Amount deposited");
+}
+
+
+export let  account2 = ( status) =>
+{
+    console.log (status);
+}
+
+
+
+export let  account3 = ( status) =>
+{
+    console.log (status);
+}
