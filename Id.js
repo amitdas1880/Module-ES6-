394cfcc16afb356842783f36d93574282454e7bf
Amit Kumar Das <102554819+amitdas1880@users.noreply.github.com>
2022-07-02 10:33:56 +0530

    Add files via upload

---
+++ Id.js
@@ -0,0 +1,39 @@
+
+let Id1 = { 
+nam : "Amit kr das" ,
+id_card : "Aadher" ,
+id_no : '9934180962' ,
+pan_no : "APJK124578" 
+}
+ 
+let Id2 = {
+nam : "Akash kr sharma",
+id : "Aadher",
+id_no : 4589632175 ,
+pan_no : "AtgK129678"
+ } 
+
+ let Id3 = {
+nam : "Omkar gupta",
+id : "Aadher",
+id_no : 7894561230 ,
+pan_no : "Auil125578"
+}
+
+
+
+export function customer1 ()
+{
+console.log (Id1)
+}
+
+export function customer2 ()
+{
+    console.log (Id2)
+}
+
+export function customer3 ()
+{
+    console.log (Id3)
+
+}
